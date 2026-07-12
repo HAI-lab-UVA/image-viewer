b5330b2b909205d04597f7133ab80a30f633552d
pigeons56 <88516772+pigeons56@users.noreply.github.com>
2026-07-12 19:02:38 -0400

    copy graph

--- graph_list_frame.py
+++ graph_list_frame.py
@@ -3,6 +3,7 @@ import tkinter.simpledialog as simpledialog
 import tkinter.messagebox as messagebox
 import settings
 import graph
+from enum import Enum
 
 class GraphListFrame(tk.Frame):
     def __init__(self, parent):
@@ -16,39 +17,75 @@ class GraphListFrame(tk.Frame):
         self.pady=3
 
         self.graphsvar = tk.StringVar()
-        self.update_graphsvar()
+        self.update_graphs("Temp") #TODO: load in all existing project graphs
         self.graphsvar.trace_add('write', self.build_graph_list)
 
         self.build_add_graph_button()
         self.build_graph_list()
+        self.build_copy_graph_button()
 
-    def update_graphsvar(self):
+    def update_graphs(self, new_graph_name):
+        settings.current_project.graphs.append(graph.Graph(new_graph_name))
         self.graphsvar.set([g.name for g in settings.current_project.graphs])
 
     def build_add_graph_button(self):
         button_add_graph = tk.Button(self, text="New Graph", justify="center", command=self.add_graph, padx=10)
         button_add_graph.grid(row=0, column=0, pady=10)
-
+    
     def build_graph_list(self, *args):
-        print("RUNNIGN!")
-        l = tk.Listbox(self, listvariable=self.graphsvar, height=20)
-        l.grid(row=1,column=0)
+        self.listbox = tk.Listbox(self, listvariable=self.graphsvar, height=20)
+        self.listbox.grid(row=1,column=0)
+
+    def build_copy_graph_button(self):
+        button_add_graph = tk.Button(self, text="Copy Selected", justify="center", command=self.copy_graph, padx=10)
+        button_add_graph.grid(row=2, column=0, pady=10)
+    
+    def build_delete_graph_button(self):
+        button_add_graph = tk.Button(self, text="Delete Selected", justify="center", command=self.delete_graph, padx=10)
+        button_add_graph.grid(row=3, column=0, pady=10)
 
     def add_graph(self):
         allow = False
-        while not allow:
+        while True:
             graph_name = simpledialog.askstring("Prompt", "Enter graph name")
             if graph_name == None:
                 return
+            graph_name = graph_name.replace(" ", "")
             allow = self.validate_graph_name(graph_name)
-        settings.current_project.graphs.append(graph.Graph(graph_name))
-        self.update_graphsvar()
+            if (allow == self.GraphValidation.SUCCESS):
+                break
+            elif (allow == self.GraphValidation.BLANK):
+                messagebox.showwarning("Warning", "Graph name cannot be blank")
+            elif (allow == self.GraphValidation.ALREADY_EXISTS):
+                messagebox.showwarning("Warning", "Graph name already exists")
+        self.update_graphs(graph_name)
+        
+    def copy_graph(self):
+        selected_indices = self.listbox.curselection()
+        print(selected_indices)
+        if (len(selected_indices) == 0): return
+        for index in selected_indices:
+            selected_graph_name = settings.current_project.graphs[index].name
+            count = 1
+            while True:
+                new_graph_name = f"{selected_graph_name} ({count})"
+                count += 1
+                allow = self.validate_graph_name(new_graph_name)
+                if (allow == self.GraphValidation.SUCCESS):
+                    break
+            self.update_graphs(new_graph_name)
+
+    def delete_graph(self):
+        pass
 
     def validate_graph_name(self, graph_name):
-        if len(graph_name.replace(" ", "")) <= 0:
-            messagebox.showwarning("Warning", "Graph name cannot be blank")
-            return False
+        if len(graph_name) <= 0:
+            return self.GraphValidation.BLANK
         if graph_name in [x.name for x in settings.current_project.graphs]:
-            messagebox.showwarning("Warning", "Graph name already exists")
-            return False
-        return True
+            return self.GraphValidation.ALREADY_EXISTS
+        return self.GraphValidation.SUCCESS
+    
+    class GraphValidation(Enum):
+        SUCCESS = 1
+        BLANK = 0
+        ALREADY_EXISTS = -1
